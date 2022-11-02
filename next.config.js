0760544848ac9dc2270fbd41d07b562ffd2ff0f3
Wahyu <wahyuief@gmail.com>
2022-11-02 23:55:47 +0700

    downgrade nextjs 12

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: 'experimental-edge',
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/json/cdn-cgi/trace',
+        destination: '/api/trace',
+      },
+    ]
+  },
+}
---
+++ pages/api/trace.js
@@ -0,0 +1,24 @@
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+export default async function handler(req) {
+  let jsonData = {};
+  const origin = req.headers.get("host");
+  
+  await fetch('https://1.1.1.1/cdn-cgi/trace')
+  .then(res => res.text())
+  .then(data => {
+    for (let entry of data.split("\n")) {
+      let pair = entry.split("=");
+      if (entry.length > 0) jsonData[pair[0]] = pair[1];
+    }
+  })
+
+  return new Response(JSON.stringify(jsonData), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+}
